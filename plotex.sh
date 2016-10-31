378c6cd513c864dd642c26cb9e6b5519a70d1bd5
andywiecko <awiecko@us.edu.pl>
2016-10-31 17:27:32 +0100

    clean

--- plotex.sh
+++ plotex.sh
@@ -4,5 +4,5 @@
 gnuplot   -e "set term epslatex color solid  font ',8' header '\\tiny'; set output 'src/fig1.tex'" $1
 jobname="${1%.*}"
 
-pdflatex  --output-directory=out --jobname=$jobname plot.tex
+pdflatex  --output-directory=out --jobname=$jobname src/plot.tex
 evince out/${jobname}.pdf &
