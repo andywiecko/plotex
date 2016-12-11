cd93705b6a32b0ba59360b56e88624afd1431052
andywiecko <awiecko@us.edu.pl>
2016-12-11 12:54:51 +0100

    shell escape enable

--- plotex.sh
+++ plotex.sh
@@ -5,8 +5,8 @@ gnuplot   -e "set term epslatex color solid  font ',8' header '\\tiny'; set outp
 jobname="${1%.*}"
 
 #pdflatex  --output-directory=out --jobname=$jobname src/plot.tex
-pdflatex  --jobname=$jobname ${PLOTEXDIR}src/plot.tex
-rm ${jobname}.aux
-rm ${jobname}.log
+pdflatex  -shell-escape --jobname=$jobname ${PLOTEXDIR}src/plot.tex 
+#rm ${jobname}.aux
+#rm ${jobname}.log
 
 evince ${jobname}.pdf &
