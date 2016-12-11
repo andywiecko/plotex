756e74a1282e25da22e2f53a803fb3031928cb73
andywiecko <awiecko@us.edu.pl>
2016-12-11 13:17:44 +0100

    adding font size by param.

--- README.md
+++ README.md
@@ -10,6 +10,15 @@ After install to plotex nice figures:
 plotex example.plt       
 ~~~
 
+By default font will have `\tiny` size, it can be changed by taking second parameter to script:
+
+~~~
+plotex example.plt \Large
+~~~
+
+It'll produce plot with font size setting to `\Large`.
+
+
 In current directory will be `example.pdf` with your figure :)
 
 ![GitHub Logo](src/example.png)
--- plotex.sh
+++ plotex.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
+ARGC=$#
+fontsize='\\tiny'
 
-gnuplot   -e "set term epslatex color solid  font ',8' header '\\tiny'; set output '${PLOTEXDIR}src/fig1.tex'" $1
-jobname="${1%.*}"
+if [ $ARGC -eq 0 ]
+then
+	echo 'filename of gnuplot script?'
+fi
 
-#pdflatex  --output-directory=out --jobname=$jobname src/plot.tex
-pdflatex  -shell-escape --jobname=$jobname ${PLOTEXDIR}src/plot.tex 
-rm ${jobname}.aux
-rm ${jobname}.log
+if [ $ARGC -gt 0 ]
+then
+	
+	if [ $ARGC -gt 1 ]
+	then
+		fontsize=$2
+		gnuplot   -e "set term epslatex color solid  font ',8' header '\\${fontsize}'; set output '${PLOTEXDIR}src/fig1.tex'" $1
+	else
+		gnuplot   -e "set term epslatex color solid  font ',8' header '\\tiny'; set output '${PLOTEXDIR}src/fig1.tex'" $1
 
-evince ${jobname}.pdf &
+	fi
+		
+	jobname="${1%.*}"
+	pdflatex  -shell-escape --jobname=$jobname ${PLOTEXDIR}src/plot.tex 
+	rm ${jobname}.aux
+	rm ${jobname}.log
+	evince ${jobname}.pdf &
+
+fi
