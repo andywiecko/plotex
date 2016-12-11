b22191df8886273101d065d7a7aa1617ce6734fd
andywiecko <awiecko@us.edu.pl>
2016-12-11 12:55:55 +0100

    rm .aux .log

--- plotex.sh
+++ plotex.sh
@@ -6,7 +6,7 @@ jobname="${1%.*}"
 
 #pdflatex  --output-directory=out --jobname=$jobname src/plot.tex
 pdflatex  -shell-escape --jobname=$jobname ${PLOTEXDIR}src/plot.tex 
-#rm ${jobname}.aux
-#rm ${jobname}.log
+rm ${jobname}.aux
+rm ${jobname}.log
 
 evince ${jobname}.pdf &
